106bcf67dd43a13b7eb8573e685cf16b813f83bd
Lucas Soffer <lsoffer@stanford.edu>
2020-06-03 21:08:51 -0400

    Prism Improvements

--- prism/args.py
+++ prism/args.py
@@ -5,12 +5,13 @@ def load_params():
             "lv" : 0.001, #hash rate of a voter chain
             "lp" : 0.1, #hash rate of a proposer chain
             "lt" : 1, #hash rate of transactions block
-            "num_voter_chains" : 10,
+            "l" : 30,
+            "num_voter_chains" : 20,
             "prop_delay" : 0.2, # in seconds
             "num_miners": 10000,
             "txPerBlock": 100,
             "communication_cost": 0.001,
             "reversal_prob": 0.001,
-            "eval_every": 500,
+            "eval_every": 10,
              }
     return params
